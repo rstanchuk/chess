fd626ca1c0d6b7f8d5edfdac0f8bc9a3fac8673b
Ivan <ivan@Ivans-MBP.fios-router.home>
2021-12-13 18:37:59 -0500

    print input

--- app/src/main/java/com/rutgers/chess/SaveFileActivity.java
+++ app/src/main/java/com/rutgers/chess/SaveFileActivity.java
@@ -3,12 +3,31 @@ package com.rutgers.chess;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SaveFileActivity extends AppCompatActivity {
-
+    Button saveButton;
+    Button cancelButton;
+    EditText editFileName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_file);
+
+        saveButton = findViewById(R.id.saveButton);
+        cancelButton = findViewById(R.id.cancelButton);
+        editFileName = findViewById(R.id.editTextFileName);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editFileName.getText().length()>0)
+                {
+                    System.out.println(editFileName.getText());
+                }
+            }
+        });
     }
 }
